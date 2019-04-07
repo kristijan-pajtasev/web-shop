6cc63d7f74ce409fd3d2f170fdc2d31d0022e0da
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-07 21:43:16 +0100

    main web shop component

---
+++ dev/WebShop/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const WebShop = () => <div>Web shop</div>
+
+export default WebShop;
--- dev/index.js
+++ dev/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebShop from './WebShop'
 
+ReactDOM.render(<WebShop/>, document.getElementById('web-shop-container'));
 
