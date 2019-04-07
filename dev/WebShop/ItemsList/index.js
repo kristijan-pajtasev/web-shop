41bbd42cfa168904b908bc8155aa1ba2e9e68af4
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-07 22:22:20 +0100

    prop types added

--- dev/WebShop/ItemsList/index.js
+++ dev/WebShop/ItemsList/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ItemsList = ({items}) => {
     const itemsList = items.map((item, index) => (
         <li key={`item-${index}`}>
             <div>{item.productName}</div>
         </li>
-    ))
+    ));
     return <ul>
         {itemsList}
     </ul>
-}
+};
+
+ItemsList.propTypes = {
+    items: PropTypes.array
+};
 
 export default ItemsList;
