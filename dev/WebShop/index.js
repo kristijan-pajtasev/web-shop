85f3e475529cda67aa11db49f56e010be59e5ed1
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-13 21:53:21 +0100

    display shopping cart and recomended items

--- dev/WebShop/index.js
+++ dev/WebShop/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ItemsList from './ItemsList';
+import ShoppingCart from './ShoppingCart';
+import RecomendedItems from './RecomendedItems';
 import items from './mocks/items';
 
 const WebShop = () => {
     return (
         <div>Web shop
-            <ItemsList items={items} />
+            <ShoppingCart items={items}/>
+            <ItemsList items={items}/>
+            <RecomendedItems items={items}/>
         </div>
     )
 };
