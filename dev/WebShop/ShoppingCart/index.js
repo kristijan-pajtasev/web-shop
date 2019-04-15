d2f91d4e8dc919c0e6e479f75c3e12fcdf4cb753
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-15 13:46:31 +0100

    cart, show amout

--- dev/WebShop/ShoppingCart/index.js
+++ dev/WebShop/ShoppingCart/index.js
@@ -2,14 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ShoppingCart = ({items}) => {
-    const itemsList = items.map((item, index) => (
+
+    const total = items.reduce((t, p) => t + parseFloat(p.price), 0);
+
+    const distinctItems = Object.entries(items.reduce((t, p) => {
+        if(!t[p.productName]) t[p.productName] = 0;
+        t[p.productName]++;
+        return t;
+    }, {})).map(e => ({
+        productName: e[0],
+        amount: e[1]
+    }));
+    console.log(distinctItems)
+
+    const itemsList = distinctItems.map((item, index) => (
         <li key={`item-${index}`}>
-            <div>{item.productName}</div>
+            <div>{item.productName} ({item.amount})</div>
         </li>
     ));
-    return <ul>
-        {itemsList}
-    </ul>
+
+    return (
+        <div>
+            <div>Cart:</div>
+
+            <ul>
+                {itemsList}
+            </ul>
+
+            <div>Total: {total}</div>
+
+            <div><button>Buy</button></div>
+        </div>
+    )
 };
 
 ShoppingCart.propTypes = {
