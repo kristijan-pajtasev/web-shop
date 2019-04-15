500f5f113a3c09757d887b78b4432f8bc3607507
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-15 14:52:07 +0100

    refactor

--- dev/WebShop/ShoppingCart/index.js
+++ dev/WebShop/ShoppingCart/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ShoppingList from './list';
 
 const ShoppingCart = ({items}) => {
 
     const total = items.reduce((t, p) => t + parseFloat(p.price), 0);
 
-    const distinctItems = Object.entries(items.reduce((t, p) => {
-        if(!t[p.productName]) t[p.productName] = 0;
-        t[p.productName]++;
-        return t;
-    }, {})).map(e => ({
-        productName: e[0],
-        amount: e[1]
-    }));
-
-    const itemsList = distinctItems.map((item, index) => (
-        <li key={`item-${index}`}>
-            <div>{item.productName} ({item.amount})</div>
-        </li>
-    ));
-
     return (
         <div>
             <div>Cart:</div>
 
-            <ul>
-                {itemsList}
-            </ul>
+            <ShoppingList products={items} />
 
             <div>Total: {total}</div>
 
