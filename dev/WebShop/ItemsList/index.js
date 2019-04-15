4eb740e7c5cad50660c97a2f8557dfdaf7cc6434
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-15 10:47:13 +0100

    add to cart action

--- dev/WebShop/ItemsList/index.js
+++ dev/WebShop/ItemsList/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ItemsList = ({items}) => {
+const ItemsList = ({items, addItemToCart}) => {
+    const addToCart = item => addItemToCart(item);
+
     const itemsList = items.map((item, index) => (
         <li key={`item-${index}`}>
             <div>{item.productName}</div>
-            <button>Add to cart</button>
+            <button onClick={addToCart.bind(this, item)}>Add to cart</button>
         </li>
     ));
     return <ul>
