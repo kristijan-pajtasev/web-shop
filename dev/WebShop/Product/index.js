e3f660b0b144d7fb27ed4ce570921d208c0c093d
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-05-30 21:16:44 +0100

    recomended product display updated

--- dev/WebShop/Product/index.js
+++ dev/WebShop/Product/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
-// product_id
-// product_category_name
-// product_name_length
-// product_description_lenght
-// product_photos_qty
-// product_weight_g
-// product_length_cm
-// product_height_cm
-// product_width_cm
+
 const Product = ({product, addProductToCart}) => {
     const {
         product_id,
