cb590f61ed302c22437fe4ccb364c9463da7fb51
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-06-02 20:30:51 +0100

    submit search query

--- dev/actions/products.js
+++ dev/actions/products.js
@@ -1,6 +1,6 @@
-export const fetchProducts = (page = 1) => {
+export const fetchProducts = (page = 1, query="") => {
     return dispatch => {
-        fetch(`http://127.0.0.1:9000/product?page=${page}`)
+        fetch(`http://127.0.0.1:9000/product?page=${page}&search=${query}`)
             .then(res => res.json()
                 .then(products => {
                     dispatch({ type: "SET_PRODUCTS", ...products })
@@ -17,5 +17,12 @@ export const fetchProduct = (productId) => {
                     dispatch({ type: "SET_PRODUCT", product })
                 })
             )
+    }
+};
+
+export const setSearchQuery = (search) => {
+    return dispatch => {
+        dispatch({ type: "SET_QUERY", search })
+        fetchProducts(1, search)(dispatch);
     }
 };
--- dev/components/Navigation/index.js
+++ dev/components/Navigation/index.js
@@ -2,29 +2,44 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getCart } from '../../actions/shopping_cart'
+import { setSearchQuery } from '../../actions/products'
 
 class Navigation extends React.PureComponent {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            search: ""
+        };
         props.actions.getCart();
+    }
+
+    setSearchValue(ev) {
+        const search = ev.target.value;
+        this.setState({ search });
+    }
+
+    setSearchQuery() {
+        this.props.actions.setSearchQuery(this.state.search);
     }
 
     render() {
         const { shoppingCart } = this.props;
+        const { search } = this.state;
         const shoppingCartSize = shoppingCart ? shoppingCart.reduce((a, b) => a + b.amount, 0) : "";
 
         return (
             <div>
                 <a href='/'>Home</a>
                 <a href='/shopping-cart'>Cart ({shoppingCartSize})</a>
+                <input type="text" value={search} onChange={this.setSearchValue.bind(this)} />
+                <button onClick={this.setSearchQuery.bind(this)}>Search</button>
             </div>
         )
     }
 }
 
 const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({ getCart }, dispatch)
+    actions: bindActionCreators({ getCart, setSearchQuery }, dispatch)
 });
 
 const mapStateToProps = state => {
