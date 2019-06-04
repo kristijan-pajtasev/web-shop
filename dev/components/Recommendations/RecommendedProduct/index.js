25004a07a331c31d07eb18700af62f56af208eea
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-06-04 10:57:37 +0100

    recommendations styling

--- dev/components/Recommendations/RecommendedProduct/index.js
+++ dev/components/Recommendations/RecommendedProduct/index.js
@@ -3,34 +3,24 @@ import React from 'react';
 const Product = ({ product, addItemToCart }) => {
     const {
         product_id,
-        product_category_name,
-        product_name_length,
-        product_description_lenght,
-        product_photos_qty,
-        product_weight_g,
-        product_length_cm,
-        product_height_cm,
-        product_width_cm
+        product_category_name
     } = product;
 
     const addToCart = () => addItemToCart(product);
 
     return (
-        <div>
-            <div>
-                <i className="material-icons">
-                    {"local_bar"}
-                </i>
+        <div className="recommended-item">
+            <div className='recommended-item-details'>
+                <div className='recommended-item-icon'>
+                    <i className="material-icons">
+                        {"local_bar"}
+                    </i>
+                </div>
+                <div className='recommended-item-data'>
+                    <div><a href={`/product/${product_id}`}>Product: {product_id}</a></div>
+                    <div>Category Name: {product_category_name}</div>
+                </div>
             </div>
-            <div><a href={`/product/${product_id}`}>Product: {product_id}</a></div>
-            <div>Category Name: {product_category_name}</div>
-            <div>Name length: {product_name_length}</div>
-            <div>Description length: {product_description_lenght}</div>
-            <div>Photos (qty): {product_photos_qty}</div>
-            <div>Weight (g): {product_weight_g}</div>
-            <div>Length (cm): {product_length_cm}</div>
-            <div>Height (cm): {product_height_cm}</div>
-            <div>Width (cm): {product_width_cm}</div>
             <button onClick={addToCart}>Add to Cart</button>
         </div>
     )
